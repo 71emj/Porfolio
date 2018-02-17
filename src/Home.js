603eb6eb081fedeed170b1ab976fe7bdaf63e2a0
71emj <tim.jeng@gmail.com>
2018-02-16 22:43:13 -0500

    minimize overscroll bug..patch not fix

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,36 @@ import Canvas from "./components/Canvas";
 import Banner from "./components/Banner";
 import "./App.css";
 
+const throttle = function(evt, scrollTimer, lastScrollFireTime, scrollEvt) {
+  const minScrollTime = 2000;
+  const now = new Date().getTime();
+
+  function processScroll() {
+    console.log(new Date().getTime().toString());
+  }
+
+  if (scrollTimer) {
+    return;
+  }
+
+  if (now - lastScrollFireTime > 3 * minScrollTime) {
+    processScroll(); // fire immediately on first scroll
+    scrollEvt(evt);
+    lastScrollFireTime = now;
+  }
+  scrollTimer = setTimeout(function() {
+    scrollTimer = null;
+    lastScrollFireTime = new Date().getTime();
+  }, minScrollTime);
+  return;
+};
+
 // the idea is, first ver of the portfolio will only contain in main page
 // portfolio link will lead to portfolio part of the page, which will only have a message
 // and links to repos
 // later will add more pages to the collections
+
+// transition won't kick in in mobile view....because the event is triggered by mousewheel
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -39,11 +65,11 @@ class Home extends Component {
     const valY = window.scrollY;
     const height = this.docHeight;
     if (scrollY) {
-      return body.style.backgroundPosition = `50% ${scrollY}%`;
+      return (body.style.backgroundPosition = `50% ${scrollY}%`);
     }
     body.style.backgroundPosition = `50% ${valY / height * 100}%`;
   };
-  
+
   scroll = (name, valY, posY) => {
     window.scrollTo(0, valY);
     this.gradientScroll(null, posY);
@@ -53,28 +79,35 @@ class Home extends Component {
         return { visible: name };
       });
     }, 300);
-  }
+  };
 
   scrollHandler = evt => {
     const { visible } = this.state;
+    const winH = window.innerHeight;
     const valY = window.scrollY;
     const height = this.docHeight;
     const flagY = evt.deltaY;
     // describing condition....
-    // if scorllY < 200 and the mouse scrolls up scroll to 0 
-    // if scrollY > 200 but < 750 
+    // if scorllY < 200 and the mouse scrolls up scroll to 0
+    // if scrollY > 200 but < 750
     switch (true) {
-      case flagY <= -35 && visible === "about":
+      case valY < 508 && flagY <= -10 && visible === "about":
         evt.preventDefault();
-        setTimeout(() => { this.scroll("home", 0, 0); }, 100);
+        setTimeout(() => {
+          this.scroll("home", 0, 0);
+        }, 100);
         break;
-      case valY > 200 && visible === "home" && flagY >= 35 || visible === "contact" && flagY <= -35:
+      case valY > 250 && visible === "home" && flagY >= 10 || visible === "contact" && flagY <= -10:
         evt.preventDefault();
-        setTimeout(() => { this.scroll("about", 745, 60); }, 100);
+        setTimeout(() => {
+          this.scroll("about", winH, 55);
+        }, 100);
         break;
-      case valY > 700 && visible === "about" && flagY >= 35:
+      case valY > 1008 && visible === "about" && flagY >= 10:
         evt.preventDefault();
-        setTimeout(() => { this.scroll("contact", 1500, 95); }, 100);
+        setTimeout(() => {
+          this.scroll("contact", winH * 2, 70);
+        }, 100);
         break;
       default:
         console.log("nothing triggerd");
@@ -84,30 +117,34 @@ class Home extends Component {
   adjustBackground = evt => {
     const { body } = this.doms;
     const height = this.docHeight;
-    body.style.backgroundSize = `${window.innerWidth}px ${height * 1.25}px`;
+    body.style.backgroundSize = `${window.innerWidth}px ${height * 2.5}px`;
   };
 
   componentDidMount() {
-    const ratio =  window.scrollY / this.docHeight;
+    const ratio = window.scrollY / this.docHeight;
     this.adjustBackground();
     window.addEventListener("resize", this.adjustBackground);
     window.addEventListener("mousewheel", evt => {
-      this.scrollHandler(evt);
+      let scrollTimer;
+      let lastScrollFireTime = 0;
+      // console.log("hhhhhhhhhhhhhhhherrrrrrrrrrrrr");
+      throttle(evt, scrollTimer, lastScrollFireTime, this.scrollHandler);
+      // this.scrollHandler(evt);
     });
 
     let field, posY;
     switch (true) {
-      case ratio <= .3:
+      case ratio <= 0.3:
         field = "home";
         posY = 0;
         break;
-      case ratio <= .6:
+      case ratio <= 0.6:
         field = "about";
-        posY = 60;
+        posY = 55;
         break;
-      case ratio <= .9:
+      case ratio <= 0.9:
         field = "contact";
-        posY = 95
+        posY = 70;
         break;
       default:
         console.log(field);
@@ -118,31 +155,28 @@ class Home extends Component {
 
   render() {
     const { visible } = this.state;
-    const links = {
-      about: "About",
-      contact: "Contact",
-      portfolio: "Portfolio",
-      source: "Github"
-    };
-
     return (
       <div>
-        <Nav
-          logo="Timothy Jeng"
-          link={{ ...links }}
-          scrollTo={this.scrollHandler}
-          key="header"
-        />
+        <Nav logo="Timothy Jeng" scrollTo={this.scrollHandler} key="header" />
         <main key="content">
           <Canvas />
-          <div className="--header" id="home">
-            <Banner src="/assets/img/log.mp4" show={visible === "home"} />
+          <div
+            className={`--content ${visible === "home" ? "" : "--hidden"}`}
+            id="home"
+          >
+            <Banner show={visible === "home"} />
           </div>
-          <div className="--header" id="about">
-            <Banner src="/assets/img/log.mp4" show={visible === "about"} />
+          <div
+            className={`--content ${visible === "about" ? "" : "--hidden"}`}
+            id="about"
+          >
+            <Banner show={visible === "about"} />
           </div>
-          <div className="--header" id="contact">
-            <Banner src="/assets/img/log.mp4" show={visible === "contact"} />
+          <div
+            className={`--content ${visible === "contact" ? "" : "--hidden"}`}
+            id="contact"
+          >
+            <Banner show={visible === "contact"} />
           </div>
         </main>
       </div>
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import Embed from "../Embed";
-import { Container, Transition } from "semantic-ui-react";
+import { Container, Transition, List } from "semantic-ui-react";
 import "./style.css";
 
 class Banner extends Component {
-
 	render() {
 		return (
 			<Container as="div" key="banner" className="--flex --wrapper">
-				<Transition animation="fade up" duration={750} visible={this.props.show}>
+				<Transition animation="fade up" visible={this.props.show} duration={{ show: 750, hide: 0 }}>
 					<div>
 						<img src="./assets/img/SVG/screen.svg" className="--monitor" />
 						<img src="./assets/img/SVG/line1.svg" className="--lines" />
@@ -21,3 +20,35 @@ class Banner extends Component {
 }
 
 export default Banner;
+
+// <Transition.Group
+// 					as={List}
+// 					animation="fade up"
+// 					duration={750}
+// 					visible={this.props.show}
+// 				>
+// 					<List.Item key="primary">
+// 						<img src="./assets/img/SVG/screen.svg" className="--monitor" />
+// 					</List.Item>
+// 					<List.Item key="first">
+// 						<img src="./assets/img/SVG/line1.svg" className="--lines" />
+// 					</List.Item>
+// 					<List.Item key="second">
+// 						<img src="./assets/img/SVG/line2.svg" className="--lines" />
+// 					</List.Item>
+// 				</Transition.Group>
+
+// <Transition.Group
+//           as={List}
+//           duration={200}
+//           divided
+//           size='huge'
+//           verticalAlign='middle'
+//         >
+//           {items.map(item => (
+//             <List.Item key={item}>
+//               <Image avatar src={`/assets/images/avatar/small/${item}.jpg`} />
+//               <List.Content header={_.startCase(item)} />
+//             </List.Item>
+//           ))}
+//         </Transition.Group>
